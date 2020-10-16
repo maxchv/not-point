796094d802ad2f33e3c8b41b62b6fe7bf1f7e189
Max Shaptala <maxshaptala@gmail.com>
2020-10-16 23:57:34 +0300

    Добавлена физика между игроком и стенами. Игровой персонаж управляется с клавиатуры.

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ const config = {
     default: 'arcade',
     arcade: {
       gravity: {
-          y: 100
-        },
-        debug: true
+        y: 100
+      },
+      debug: true
     },
-}
+  }
 };
 
 const game = new Phaser.Game(config);
@@ -32,21 +32,40 @@ function preload() {
 }
 
 let player;
+let cursors;
 
-function create() { 
+function create() {
 
- 
-  const map = this.make.tilemap({ key: 'map', tileWidth: 64, tileHeight: 64 });
+
+  const map = this.make.tilemap({
+    key: 'map',
+    tileWidth: 64,
+    tileHeight: 64
+  });
   const tiles = map.addTilesetImage('tilesetll', "tiles");
 
   const wall = map.createStaticLayer('wall', tiles, 0, 0);
   const floor = map.createStaticLayer('floor', tiles, 0, 0);
+  wall.setCollisionByExclusion(-1, true);
 
   player = this.physics.add.sprite(96, 96, 'player');
+  player.setBounce(0.1);
   player.setCollideWorldBounds(true);
   this.physics.add.collider(player, wall);
+
+  cursors = this.input.keyboard.createCursorKeys();
 }
 
 function update() {
+  if (cursors.left.isDown) {
+    player.setVelocityX(-200);
+  } else if (cursors.right.isDown) {
+    player.setVelocityX(200);
+  } else {
+    player.setVelocityX(0);
+  }
 
+  if ((cursors.space.isDown || cursors.up.isDown)) {
+    player.setVelocityY(-350);
+  }
 }
